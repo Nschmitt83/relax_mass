e3f3b394d6cfbe11db3b5817f61427d6d0cfe63f
nicolas schmitt <schmitt_953@hotmail.com>
2019-08-27 18:11:46 +0200

    cartes terminées

--- config/initializers/massage_type.rb
+++ config/initializers/massage_type.rb
@@ -1,19 +1,33 @@
 MASSAGE_PRICE_AND_TYPE = {
   "Massage Suedois" => {
     price: 50,
-    description: "",
-    photo: "",
+    description: "Le massage suédois tire ses enseignements de la médecine traditionnelle et ancestrale chinoise. Il est considéré comme étant le plus classique des massages de bien-être. Le massage suédois réunit les méthodes de bases du massage de bien-être. Très relaxant, il se compose de manœuvres douces et de gestes fluides et longs réalisés sur le corps entier. Les cinq mouvements de base sont : la percussion, l'effleurage, le pétrissage, la friction réalisée entre le cou et l'épaule, ainsi que le malaxage ou pour certains le tapotement et la vibration.",
+    photo: "https://res.cloudinary.com/dvmytrwrd/image/upload/c_fit,h_375,w_420/v1566913503/iris-formations-massage-suedois_ldbxim.jpg",
   },
-  "Massage californien" => {
-    price: 45
+  "Massage Californien" => {
+    price: 45,
+    description: "  Plus communément appelé le « toucher du cœur », le massage californien est une pratique psycho-corporelle datant des années 70. Dans cette fiche, vous découvrirez en quoi ce massage consiste, ses principes, son histoire, ses bienfaits, le déroulement d’une séance, comment le pratiquer et ses contre-indications.
+    Le massage californien est une approche globale qui vise autant la détente que l'éveil d'une conscience psychocorporelle. Ce massage utilise de longs mouvements lents et fluides qui permettent une profonde relaxation physique et psychique.",
+    photo: "https://res.cloudinary.com/dvmytrwrd/image/upload/c_fit,h_375,w_420/v1566914024/p1433-750x459-le-monde-de-na-au-1_vr3fwi.jpg",
+
   },
   "Shiatsu" => {
-    price: 60
+    price: 60,
+    description: "Thérapie officiellement reconnue au Japon, très prisée des cadres à la sortie des bureaux, le shiatsu apaise, redonne vitalité et dynamisme en une séance de 45 minutes.
+    Pratiqué sur un tapis de sol, à travers les vêtements, ce massage par pression des doigts le long des méridiens d'acupuncture débloque l'énergie des zones corporelles contractées, mais aussi le mental en redonnant confiance. Il se fait tonique le matin et plus calmant le soir. Il se pratique également sur une chaise, dans certains réseaux (Touch Line), travaillant plus particulièrement les épaules, le dos, la nuque, les bras, les mains, la tête et les hanches.",
+    photo: "https://res.cloudinary.com/dvmytrwrd/image/upload/c_fit,h_375,w_420/v1566918902/shiatsu-768x480_khvwre.jpg",
+
   },
   "Pierres chaudes" => {
-    price: 80
+    price: 80,
+    description: "Selon plusieurs sources, l'utilisation des pierres chaudes pour le bien-être et les soins remonterait à l’antiquité. Dans différentes parties du monde, les pierres chaudes sont utilisées pour soulager, comme en Chine il y a plus de 4000 ans, mais aussi au Japon lors du massage amma assis (une des formes les plus anciennes du massage en Asie). Les pierres chaudes sont également présentes en Afrique lors de rituels curatifs, ainsi qu'en Europe pour chauffer les saunas par exemple, en Polynésie où les pierres sont utilisées pour leurs vertus curatives et sur le continent Amérindien pour chasser la fatigue et stimuler la circulation.",
+    photo: "https://res.cloudinary.com/dvmytrwrd/image/upload/c_fit,h_375,w_420/v1566914230/ezgif-1-de121fe2eb_syna2o.jpg",
+
   },
   "Réflexologie" => {
-    price: 70
+    price: 70,
+    description: "  La réflexologie vise à mobiliser les processus d'autoguérison du corps. Elle se pratique en exerçant une pression soutenue, avec le bout des doigts, sur des zones ou des points réflexes situés sur les pieds, les mains et les oreilles. Ces points correspondent à des organes ou des fonctions organiques. Elle cherche plutôt à apporter un certain soulagement à toutes sortes d’affections : maux de tête ou de dos, syndrome prémenstruel, stress, troubles respiratoires, etc.",
+    photo: "https://res.cloudinary.com/dvmytrwrd/image/upload/c_fit,h_375,w_420/v1566918972/cabinet-reflexologie-montelimar-1200x795_xpzsid.jpg",
+
   },
 }
