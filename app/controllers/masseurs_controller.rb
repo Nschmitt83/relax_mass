bb7bdb96714d170f1bd1df489807bf8825d63d96
marine monbailly <marinemonbailly@gmail.com>
2019-08-28 10:15:33 +0200

    card masseur et color

--- app/controllers/masseurs_controller.rb
+++ app/controllers/masseurs_controller.rb
@@ -1,8 +1,11 @@
 class MasseursController < ApplicationController
   def index
     @masseurs = User.where(mass_or_not: true)
+    @reviews = @masseurs.reviews
   end
 
   def show
+    @masseur = User.find(params[:id])
+    @reviews = @masseur.reviews
   end
 end
