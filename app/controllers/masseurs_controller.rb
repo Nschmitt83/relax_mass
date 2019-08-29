94e4dbce6f5409a1f8e91f04e5459a0bcb15d8ea
Faustine Fourdinier <lafaustinbox@gmail.com>
2019-08-29 17:00:18 +0200

    wip

--- app/controllers/masseurs_controller.rb
+++ app/controllers/masseurs_controller.rb
@@ -1,35 +1,15 @@
 class MasseursController < ApplicationController
   def index
     if params[:user].present?
-      sql_query = " \
-      massages.massage_type ILIKE :type \
-      AND users.city ILIKE :city \
-      AND users.gender ILIKE :gender \
-      "
-      @masseurs = User.joins(:massages).where(sql_query,
-                                              type: "%#{params[:user][:first_name]}%",
-                                              city: "%#{params[:user][:city]}%",
-                                              gender: "%#{params[:user][:gender]}%").uniq
-      @masseurs = @masseurs.select { |masseur| masseur.rating.to_i >= params[:user][:last_name].to_i } if params[:user][:last_name].present?
+      refine
+    elsif params[:search].present?
+      home_search
     else
       @masseurs = User.where(mass_or_not: true)
     end
-    # @title_inject = "A #{params["city"].capitalize}"
     @reviews = @masseurs.map(&:reviews)
     @massage = Massage.new
     @user    = User.new
-
-    if params["search"].present?
-      sql_query = " \
-      massage_type ILIKE :type \
-      AND city ILIKE :city \
-      "
-      @masseurs = User.joins(:massages).where(sql_query,
-                                              type: "%#{params["search"][:massage_type]}%",
-                                              city: "%#{params["search"][:city]}%").uniq
-    else
-      @masseurs = User.where(mass_or_not: true)
-    end
   end
 
   def show
@@ -37,5 +17,32 @@ class MasseursController < ApplicationController
     @reviews = @masseur.reviews
     @booking = Booking.new
     @average = @reviews.sum(&:rating) / @reviews.size.to_f
+  end
+
+  private
+
+  def refine
+    sql_query = " \
+      massages.massage_type ILIKE :type \
+      AND users.city ILIKE :city \
+      AND users.gender ILIKE :gender \
+    "
+    @masseurs = User.joins(:massages).where(sql_query,
+                                            type: "%#{params[:user][:first_name]}%",
+                                            city: "%#{params[:user][:city]}%",
+                                            gender: "%#{params[:user][:gender]}%").uniq
+    @masseurs = @masseurs.select { |masseur| masseur.rating.to_i >= params[:user][:last_name].to_i } if params[:user][:last_name].present?
+    @massage_type = params[:user][:first_name]
+  end
+
+  def home_search
+    sql_query = " \
+      massage_type ILIKE :type \
+      AND city ILIKE :city \
+    "
+    @masseurs = User.joins(:massages).where(sql_query,
+                                            type: "%#{params[:search][:massage_type]}%",
+                                            city: "%#{params[:search][:city]}%").uniq
+    @massage_type = params[:search][:massage_type]
   end
 end
