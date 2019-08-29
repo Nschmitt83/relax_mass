684132d266520441df595e00435ea4a66486e50c
marine monbailly <marinemonbailly@gmail.com>
2019-08-29 15:59:11 +0200

    save

--- app/controllers/masseurs_controller.rb
+++ app/controllers/masseurs_controller.rb
@@ -3,14 +3,11 @@ class MasseursController < ApplicationController
     @masseurs = User.where(mass_or_not: true)
     @reviews = @masseurs.map(&:reviews)
     @massages = Massage.all
-    @massages = Massage.all
-
   end
 
   def show
     @masseur = User.find(params[:id])
     @reviews = @masseur.reviews
     @booking = Booking.new
-    @average = @reviews.sum(&:rating) / @reviews.size.to_f
   end
 end
