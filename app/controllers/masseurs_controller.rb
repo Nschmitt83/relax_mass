a3a1c592038036d9e48ff947730e79335d36c507
Faustine Fourdinier <lafaustinbox@gmail.com>
2019-08-29 16:08:59 +0200

    save form hp

--- app/controllers/masseurs_controller.rb
+++ app/controllers/masseurs_controller.rb
@@ -1,10 +1,14 @@
 class MasseursController < ApplicationController
   def index
     @masseurs = User.where(mass_or_not: true)
-    @reviews = @masseurs.map(&:reviews)
-    @massages = Massage.all
+    @reviews  = @masseurs.map(&:reviews)
     @massages = Massage.all
 
+    if params["search"].present?
+      @masseurs = User.where("city ILIKE ?", params["search"]["city"])
+    else
+      @masseurs = User.where(mass_or_not: true)
+    end
   end
 
   def show
