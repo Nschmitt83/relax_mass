36bc5470557346d7d955c5803556f28fec8d53a0
marine monbailly <marinemonbailly@gmail.com>
2019-08-29 13:22:56 +0200

    modfi navbar

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,20 @@
 class BookingsController < ApplicationController
   def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @masseur = User.find(params[:masseur_id])
+    @massage = @masseur.massages.where(massage_type: params[:booking][:massage])
+    @booking.massage = @massage.first
+    if @booking.save
+      redirect_to masseur_path(@masseur)
+    else
+      render masseur_path(@masseur)
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date)
   end
 end
