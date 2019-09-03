919b3299cf66aefb762b766b7a448d99341c168c
nicolas schmitt <schmitt_953@hotmail.com>
2019-09-03 16:39:26 +0200

    front finalisé

---
+++ app/javascript/plugins/stripe.js
@@ -0,0 +1,11 @@
+const actual_payment_modal = document.querySelector('.Checkout.is-desktop');
+const wanted_payment_modal = document.querySelector('.wanted_payment_modal');
+
+if(actual_payment_modal) {
+  actual_payment_modal.addEventListener('change', (event) => {
+    wanted_payment_modal.innerText = `${actual_payment_modal.value}`;
+  })
+}
+
+console.log(actual_payment_modal.value);
+console.log(wanted_payment_modal.value);
