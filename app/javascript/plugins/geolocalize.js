e7a8484195f3bc25fa4f126a9d845b8820c37a98
nicolas schmitt <schmitt_953@hotmail.com>
2019-09-05 10:08:02 +0200

    back fini

--- app/javascript/plugins/geolocalize.js
+++ app/javascript/plugins/geolocalize.js
@@ -1,11 +1,20 @@
-// const currentLocationLink = document.getElementById('current-location');
-// console.log(currentLocationLink);
-// currentLocationLink.addEventListener('click', (event) => {
-//     console.log('test inside');
-//     event.preventDefault();
-//     navigator.geolocation.getCurrentPosition((data) => {
-//     console.log(data.coords.latitude);
-//     console.log({ lat: data.coords.latitude, lon: data.coords.longitude });
-//   });
-// });
-// console.log('test outside');
+const apiKey = "2c27197fe81e40ad82eb2bb38f1f5146";
+const currentLocationLink = document.getElementById('current-location');
+const cityForm = document.getElementById('city_form');
+
+const fetchCity = (coordinates) => {
+  fetch(`https://api.opencagedata.com/geocode/v1/json?q=${coordinates.lat}+${coordinates.lon}&key=${apiKey}`)
+  .then(response => response.json())
+  .then(data => {
+    const city = data.results[0].components.city;
+    cityForm.value = city;
+  });
+};
+
+currentLocationLink.addEventListener('click', (event) => {
+    event.preventDefault();
+    navigator.geolocation.getCurrentPosition((data) => {
+    const coordinates = { lat: data.coords.latitude, lon: data.coords.longitude };
+    fetchCity(coordinates);
+  });
+});
